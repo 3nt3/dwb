ac67a44b22cc6d655a6a18e6203bd482f222fd8f
Niels Dingsbums <niels.dingsbums@gmx.de>
2019-01-19 13:22:22 +0100

    stuff

---
+++ models/item.go
@@ -0,0 +1,60 @@
+package models
+
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/gobuffalo/pop"
+	"github.com/gobuffalo/pop/nulls"
+	"github.com/gobuffalo/uuid"
+	"github.com/gobuffalo/validate"
+	"github.com/gobuffalo/validate/validators"
+)
+
+type Item struct {
+	ID          uuid.UUID    `json:"id" db:"id"`
+	CreatedAt   time.Time    `json:"created_at" db:"created_at"`
+	UpdatedAt   time.Time    `json:"updated_at" db:"updated_at"`
+	DueDate     time.Time    `json:"due_date" db:"due_date"`
+	Class       string       `json:"class" db:"class"`
+	Exercise    string       `json:"exercise" db:"exercise"`
+	Information nulls.String `json:"information" db:"information"`
+	Author      string       `json:"author" db:"author"`
+}
+
+// String is not required by pop and may be deleted
+func (i Item) String() string {
+	ji, _ := json.Marshal(i)
+	return string(ji)
+}
+
+// Items is not required by pop and may be deleted
+type Items []Item
+
+// String is not required by pop and may be deleted
+func (i Items) String() string {
+	ji, _ := json.Marshal(i)
+	return string(ji)
+}
+
+// Validate gets run every time you call a "pop.Validate*" (pop.ValidateAndSave, pop.ValidateAndCreate, pop.ValidateAndUpdate) method.
+// This method is not required and may be deleted.
+func (i *Item) Validate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.Validate(
+		&validators.StringIsPresent{Field: i.Class, Name: "Class"},
+		&validators.StringIsPresent{Field: i.Exercise, Name: "Exercise"},
+		&validators.StringIsPresent{Field: i.Author, Name: "Author"},
+	), nil
+}
+
+// ValidateCreate gets run every time you call "pop.ValidateAndCreate" method.
+// This method is not required and may be deleted.
+func (i *Item) ValidateCreate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
+
+// ValidateUpdate gets run every time you call "pop.ValidateAndUpdate" method.
+// This method is not required and may be deleted.
+func (i *Item) ValidateUpdate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
